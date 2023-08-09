2cda702e8f2cfac9ae13ff6003a812373861b666
majed <majedhabli2020@gmail.com>
2023-08-09 20:19:02 +0300

    Created signin.jsx and started implementing the skeletal structure of the page.

--- front-end/instagram_clone/src/pages/siginin/signin.jsx
+++ front-end/instagram_clone/src/pages/siginin/signin.jsx
@@ -3,7 +3,17 @@ import styles from './signin.module.css';
 
 const Signin = () =>{
     return (
-        <div className={styles.container}></div>
+        <div className={styles.container}>
+            <div className={styles.container_body}>
+                <div className={styles.body_left}>
+                    <div className={styles.image_container}>
+                        <img src="/leftIphoneImage.png" alt="left_iphone_image" />
+                        <img src="/rightIphoneImage.png" alt="right_iphone_image" />
+                    </div>
+                </div>
+                <div className={styles.body_right}></div>
+            </div>
+        </div>
     )
 }
 
