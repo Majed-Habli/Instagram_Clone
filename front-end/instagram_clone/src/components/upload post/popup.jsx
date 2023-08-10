082065374310805c634bb5b62836e84bfed2e48f
majed <majedhabli2020@gmail.com>
2023-08-10 15:38:33 +0300

    Created pop up component with initial styling.

---
+++ front-end/instagram_clone/src/components/upload post/popup.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from  './popup.module.css';
+
+export const Popup = () =>{
+    return (
+        <div className={styles.container}>
+            <div className={styles.popup_container}></div>
+        </div>
+    )
+}
