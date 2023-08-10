0eb362404919ea566d1750ab682757dac2cfb9b8
majed <majedhabli2020@gmail.com>
2023-08-10 23:51:51 +0300

    Updated the searchbar styling.

--- front-end/instagram_clone/src/components/searchbar/search.jsx
+++ front-end/instagram_clone/src/components/searchbar/search.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './search.module.css';
 import { Card } from "../shared/user card/userCard";
 
-export const Searchbar = () =>{
+export const Searchbar = ({isPressed}) =>{
+    const [search, setSearch] = useState('');
     const buttonText = "follow";
+
+    const hideSearchModel =() =>{
+        isPressed(prev => !prev);
+    }
 
     return(
         <div className={styles.container}>
             <div className={styles.container_header}>
-                <div>Search</div>
-                <input type="text" placeholder="Search" />
+                <div className={styles.title}>
+                    <div>Search </div>
+                    <img src="/x.png" alt="close image" onClick={hideSearchModel}/>
+                </div>
+                <input type="text" placeholder="Search" value={search} onChange={ e=> setSearch(e.target.value)}/>
             </div>
             <div className={styles.body}>
                 <Card button_title={buttonText}/>
