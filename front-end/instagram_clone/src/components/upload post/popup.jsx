63e9c680913b4fb38bcb20731df9c864531fa378
majed <majedhabli2020@gmail.com>
2023-08-10 19:53:17 +0300

    Updated the popup styling.

--- front-end/instagram_clone/src/components/upload post/popup.jsx
+++ front-end/instagram_clone/src/components/upload post/popup.jsx
@@ -10,7 +10,20 @@ export const Popup = ({isPressed}) =>{
     return (
         <div className={styles.container}>
             <div className={styles.popup_container}>
-                <img src="/x.png" alt="close image" onClick={hideModel}/>
+                {/* <img src="/x.png" alt="close image" onClick={hideModel}/> */}
+                <div className={styles.popup_header}>
+                    Create new post
+                </div>
+                <div className={styles.popup_body}>
+                    <img src="/upload_bg.png" alt="" />
+                    <div>Dragg photos and videos here</div>
+                    <div className={styles.btn}>
+                        <div className={styles.btn_container}>
+                            <input type="file" name="upload_file[]" id="upload_file" multiple="multiple" class="form-control" hidden/>
+                            <label className={styles.upload_label} for="upload_file">Select from computer</label>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
